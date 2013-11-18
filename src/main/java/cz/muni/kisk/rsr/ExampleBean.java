a336709c0eb018338ae0b50647df6ab95b21f9eb
Vaclav Rosecky <xrosecky@gmail.com>
2013-11-18 14:22:01 +0100

    json serialization

---
+++ src/main/java/cz/muni/kisk/rsr/ExampleBean.java
@@ -0,0 +1,29 @@
+package cz.muni.kisk.rsr;
+
+/**
+ *
+ * @author xrosecky
+ */
+public class ExampleBean {
+
+    private String name;
+    
+    private int length;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+    
+}
--- src/main/java/cz/muni/kisk/rsr/RSRServlet.java
+++ src/main/java/cz/muni/kisk/rsr/RSRServlet.java
@@ -5,11 +5,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.codehaus.jackson.map.ObjectMapper;
 
 public class RSRServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getOutputStream().print("OK");
+        ObjectMapper mapper = new ObjectMapper();
+        ExampleBean example = new ExampleBean();
+        example.setName("test");
+        example.setLength(10);
+        mapper.writeValue(response.getOutputStream(), example);
+        //response.getOutputStream().print("OK");
     }
 }
