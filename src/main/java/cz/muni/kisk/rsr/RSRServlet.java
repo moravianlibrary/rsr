04105f5b4cad9d107d56c428114197780883a28d
Vaclav Rosecky <xrosecky@gmail.com>
2013-11-18 14:44:12 +0100

    pretty printing of response

--- src/main/java/cz/muni/kisk/rsr/RSRServlet.java
+++ src/main/java/cz/muni/kisk/rsr/RSRServlet.java
@@ -1,6 +1,9 @@
 package cz.muni.kisk.rsr;
 
+import cz.muni.kisk.rsr.tokenizer.AuthorityRecord;
+import cz.muni.kisk.rsr.tokenizer.AuthorityTokenizer;
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,10 +15,14 @@ public class RSRServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ObjectMapper mapper = new ObjectMapper();
+        /*
         ExampleBean example = new ExampleBean();
         example.setName("test");
         example.setLength(10);
-        mapper.writeValue(response.getOutputStream(), example);
+        */
+        List<AuthorityRecord> authorities =  AuthorityTokenizer.getAuthorityRecords("Skusobny text. Jan Novak");
+        mapper.defaultPrettyPrintingWriter().writeValue(response.getOutputStream(), authorities);
+        //mapper.writeValue(response.getOutputStream(), authorities);
         //response.getOutputStream().print("OK");
     }
 }
